dd5de27343e18584971da527d216b7291f551ef8
antonionduarte <an.duarte@campus.fct.unl.pt>
2023-03-02 03:21:26 +0000

    removed useless class

--- src/banking-apis/cgd.py
+++ src/banking-apis/cgd.py
@@ -13,6 +13,8 @@ NORDIGEN_API_USER_AGREEMENT = "agreements/enduser/"
 NORDIGEN_BANK_ID = ""
 NORDIGEN_COUNTRY_CODE_PORTUGAL = "pt"
 
+REFERENCE_ID = "123456"
+
 CAIXA_GERAL_DEPOSITOS_ID = "CAIXA_GERAL_DEPOSITOS_CGDIPTPL"
 
 nordigen_key = dotenv.get_key(dotenv_path=".env", key_to_get=NORDIGEN_SECRET_KEY_ENV)
@@ -26,103 +28,113 @@ TODO:
 
 """
 
+def request_access_token():
+    url = NORDIGEN_API + NORDIGEN_API_NEW_TOKEN
 
-""" TODO: These two classes should probably be global and shared amongst the several APIs """
-class TransactionHistory:
+    headers = {
+        'accept': 'application/json',
+        'Content-Type': 'application/json'
+    }
+
+    data = {
+        'secret_id': nordigen_id,
+        'secret_key': nordigen_key
+    }
+
+    json_data = json.dumps(data)
+
+    response = requests.post(url, headers=headers, data=json_data)
+
+    if not response.ok:
+        raise Exception(f"ERROR: Error performing CGDApi access token request: {response.text}")
+
+    return json.loads(response.text)
+
+
+def request_end_user_agreement():
+    access_key = request_access_token()['access']
+
+    url = NORDIGEN_API + NORDIGEN_API_USER_AGREEMENT
+
+    headers = {
+        'accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': f'Bearer {access_key}',
+    }
+
+    data = {
+        'institution_id': CAIXA_GERAL_DEPOSITOS_ID,
+        'max_historical_days': '90',
+        'access_valid_for_days': '90',
+        'access_scope': ['balances', 'details', 'transactions']
+    }
+
+    json_data = json.dumps(data)
+
+    response = requests.post(url, headers=headers, data=json_data)
+
+    if not response.ok:
+        raise Exception(f"ERROR: Error performing CGDApi user agreement: {response.text}")
+
+    return json.loads(response.text)
+
+
+def request_build_link(user_agreement):
+    access_key = request_access_token()['access']
+
+    url = ""
+
+    headers = {
+        'accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': f'Bearer {access_key}',
+    }
+
+    # TODO: Complete this.
+
+    data = {
+        'redirect': 'http://www.google.com',
+        'institution_id': CAIXA_GERAL_DEPOSITOS_ID,
+        'reference': REFERENCE_ID,
+        'agreement': {user_agreement},
+        'user_language': 'EN'
+    }
+
     pass
 
-class AccountBalance:
+
+def request_transaction_history():
+    access_key = request_access_token()
+
     pass
 
-class CGDApi:
-    def __init__(self):
-        pass
+
+def request_bank_list():
+    access_key = request_access_token()['access']
+
+    url = NORDIGEN_API + NORDIGEN_API_COUNTRY_BANKS.format(country_code=NORDIGEN_COUNTRY_CODE_PORTUGAL)
+
+    headers = {
+        'accept': 'application/json',
+        'Authorization': f'Bearer {access_key}'
+    }
+
+    response = requests.get(url, headers=headers)
+
+    if not response.ok:
+        raise Exception(f"Error: Error performing CGDApi request bank list: {response.text}")
+
+    return json.loads(response.text)
 
 
-    def request_access_token(self):
-        url = NORDIGEN_API + NORDIGEN_API_NEW_TOKEN
+def request_account_balance():
+    access_key = request_access_token()
 
-        headers = {
-            'accept': 'application/json',
-            'Content-Type': 'application/json'
-        }
-
-        data = {
-            'secret_id': nordigen_id,
-            'secret_key': nordigen_key
-        }
-
-        json_data = json.dumps(data)
-
-        response = requests.post(url, headers=headers, data=json_data)
-
-        if not response.ok:
-            raise Exception(f"ERROR: Error performing CGDApi access token request: {response.text}")
-
-        return json.loads(response.text)
-
-
-    def request_end_user_agreement(self):
-        access_key = self.request_access_token()['access']
-
-        url = NORDIGEN_API + NORDIGEN_API_USER_AGREEMENT
-
-        headers = {
-            'accept': 'application/json',
-            'Content-Type': 'application/json',
-            'Authorization': f'Bearer {access_key}',
-        }
-
-        data = {
-            'institution_id': CAIXA_GERAL_DEPOSITOS_ID,
-            'max_historical_days': '90',
-            'access_valid_for_days': '90',
-            'access_scope': ['balances', 'details', 'transactions']
-        }
-
-        json_data = json.dumps(data)
-
-        response = requests.post(url, headers=headers, data=json_data)
-
-        if not response.ok:
-            raise Exception(f"ERROR: Error performing CGDApi user agreement: {response.text}")
-
-        return json.loads(response.text)
-
-
-    def request_transaction_history(self):
-        access_key = self.request_access_token()
-    
-        pass
-
-    
-    def request_bank_list(self):
-        access_key = self.request_access_token()['access']
-
-        url = NORDIGEN_API + NORDIGEN_API_COUNTRY_BANKS.format(country_code=NORDIGEN_COUNTRY_CODE_PORTUGAL)
-
-        headers = {
-            'accept': 'application/json',
-            'Authorization': f'Bearer {access_key}'
-        }
-
-        response = requests.get(url, headers=headers)
-
-        if not response.ok:
-            raise Exception(f"Error: Error performing CGDApi request bank list: {response.text}")
-
-        return json.loads(response.text)
-
-
-    def request_account_balance(self):
-        access_key = self.request_access_token()
-
-        pass
+    pass
 
 
 if __name__ == "__main__":
     """ Main function for testing """
-    api = CGDApi()
     # access_token = api.request_access_token()
     # banks = api.request_bank_list()
 
@@ -130,4 +142,5 @@ if __name__ == "__main__":
     # file = open('banks.json', 'w')
     # file.write(banks_str)
     # file.close()
-    print(api.request_end_user_agreement())
+    # print(api.request_end_user_agreement())
+    print(request_end_user_agreement())
